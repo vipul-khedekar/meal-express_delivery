8dd1c8de5838e9c31c35f89a290a57af9c309ad8
vipul-khedekar <3kvipul@gmail.com>
2022-08-20 22:58:47 +0530

    Update: Imported main component and style file to root.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react";
+
+import "./index.css";
+import App from "./App";
+
+const root = document.querySelector(`#root`);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
