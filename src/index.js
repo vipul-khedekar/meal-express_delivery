aad2179687cbf59f8b36acc1bf49686b40e5aac8
vipul-khedekar <3kvipul@gmail.com>
2022-08-21 19:02:15 +0530

    Update: Wrapped context provider for root component, so that props can be passed in any sub components.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import { reducer } from "./context/reducer";
 
 const root = document.querySelector(`#root`);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </BrowserRouter>,
   root
 );
