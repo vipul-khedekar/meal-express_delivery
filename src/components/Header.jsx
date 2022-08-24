ffd7290b6e56a5b2f2b9c1eb509c91b5739d32a7
vipul-khedekar <3kvipul@gmail.com>
2022-08-24 20:05:06 +0530

    Feature: responsive Mobile view. Narrow header height. Fix: Cursor pointer on all options. Code structure.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,8 +29,8 @@ function Header() {
   }
 
   return (
-    <header className='box-border w-screen bg-slate-300 fixed z-50 p-6 px-14'>
-        <div className='hidden md:flex h-full w-full justify-between items-center'>
+    <header className='box-border w-screen bg-slate-300 fixed z-50 p-6 px-8'>
+        <div className='hidden md:flex h-[5vh] w-full justify-between items-center'>
           <Link to={"/"}>
             <p className='text-2xl font-bold'>Meal <span className='italic'>Express...</span></p>
           </Link>
@@ -46,19 +46,48 @@ function Header() {
             <div className='h-4 w-4 absolute top-0 left-4 rounded-full bg-black flex justify-center items-center'>
               <p className='text-xs text-white font-semibold'>2</p>
             </div>
+
             <img className='h-9 w-9 rounded-full relative drop-shadow' onClick={() => loginAndMenu()} src={user ? user.photoURL : Avatar} alt={user ? user.displayName : "user-avatar"} />
+
             {isMenu && (
               <menu className='flex flex-col bg-yellow-200 w-36 rounded-lg absolute top-10 drop-shadow text-center'>
                 <Link to={"/create-item"}>
                   <p className='p-1 rounded-lg hover:bg-red-600'>Add new item</p>
                 </Link>
+
                 <p className='p-1 rounded-lg hover:bg-red-600'>Logout</p>
               </menu>
             )}
           </div>
         </div>
 
-        <div className='flex md:hidden'>Mobile</div>
+        <div className='flex md:hidden h-[4vh] w-full justify-between items-center'>
+          <Link to={"/"}>
+            <p className='text-2xl font-bold'>Meal <span className='italic'>Express...</span></p>
+          </Link>
+
+          <div className='flex justify-center items-center gap-7 relative'>
+            <MdOutlineShoppingCart className='text-xl cursor-pointer' />
+            
+            <div className='h-4 w-4 absolute top-0 left-4 rounded-full bg-black flex justify-center items-center'>
+              <p className='text-xs text-white font-semibold'>2</p>
+            </div>
+            
+            <img className='h-9 w-9 rounded-full relative drop-shadow cursor-pointer' onClick={() => loginAndMenu()} src={user ? user.photoURL : Avatar} alt={user ? user.displayName : "user-avatar"} />
+            
+            {isMenu && (
+              <menu className='flex flex-col gap-2 bg-yellow-200 w-36 rounded-lg absolute top-10 drop-shadow text-center'>
+                <Link to={"/create-item"}>
+                  <p className='p-1 rounded-lg cursor-pointer'>Add new item</p>
+                </Link>
+                <p className='p-1 rounded-lg cursor-pointer'>Home</p>
+                <p className='p-1 rounded-lg cursor-pointer'>Menu</p>
+                <p className='p-1 rounded-lg cursor-pointer'>Services</p>
+                <p className='p-1 rounded-lg cursor-pointer'>Logout</p>
+              </menu>
+            )}
+          </div>
+        </div>
     </header>
   )
 }
