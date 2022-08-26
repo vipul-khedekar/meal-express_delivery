55ceb432180f8197c9d9ea7360c2d757680934e9
vipul-khedekar <3kvipul@gmail.com>
2022-08-26 17:17:53 +0530

    Update: Responsive mobile view. Scroll down instruction for PC view.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiChevronsDown } from "react-icons/fi";
 
 import HeroBackground from "../images/hero-background.png";
 import { heroItems } from "../utils/heroItemsData";
@@ -25,6 +26,11 @@ function Hero() {
           <button className="bg-red-600 rounded-full py-2 px-5 font-bold text-white text-2xl">
             Order Now
           </button>
+
+          <div className="flex justify-center items-center gap-2 mt-20">
+            <p className="text-xl">Scroll Down</p>
+            <FiChevronsDown className="text-3xl" />
+          </div>
         </div>
 
         <div className="flex-1 flex justify-center mt-14 ml-24">
@@ -54,7 +60,49 @@ function Hero() {
         </div>
       </div>
 
-      <div className="flex md:hidden">Mobile</div>
+      <div className="flex flex-col justify-center items-center gap-8 w-[100vw] px-5 mt-20 md:hidden">
+        <div className="flex-1 flex flex-col justify-center items-start gap-5">
+          <p className="font-bold text-4xl tracking-wide leading-loose">
+            Quick <span className="text-red-700">food</span> delivery <br />
+            in <span className="text-red-700">12</span> minutes,
+            <br />
+            right <span className="text-red-700">here</span>!
+          </p>
+
+          <p className="mt-[-20px] italic font-extralight">
+            *Conditions apply.
+          </p>
+          <p className="mt-[-20px] italic font-extralight">
+            *Order at your own risk.
+          </p>
+        </div>
+
+        <button className="bg-red-600 rounded-full py-2 px-7 font-bold text-white text-2xl">
+          Order Now
+        </button>
+
+        <div className=" flex flex-col justify-center items-center m-[-3rem] mt-0 relative">
+          <img src={HeroBackground} alt="hero-background" />
+
+          <div className="flex flex-col justify-center items-center gap-6 absolute top-5 left-14">
+            {heroItems &&
+              heroItems.map((item) => {
+                return (
+                  <div
+                    key={item.id}
+                    className="flex justify-center items-center"
+                  >
+                    <img className="w-24" src={item.picture} alt={item.name} />
+                    <div className="flex flex-col w-[340px] mt-10">
+                      <p className="font-bold text-xl">{item.name}</p>
+                      <p>₹{item.price}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
