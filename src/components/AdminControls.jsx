c00ffb8acdb990e647c94ab9ee1db00187bfd116
vipul-khedekar <3kvipul@gmail.com>
2022-08-27 15:16:01 +0530

    Update: Stylized mobile view.

--- src/components/AdminControls.jsx
+++ src/components/AdminControls.jsx
@@ -133,7 +133,7 @@ function AdminControls() {
 
   return (
     <div className="w-full px-8 mt-24">
-      <div className="flex flex-col justify-center items-center gap-4">
+      <div className="hidden md:flex flex-col justify-center items-center gap-4">
         {isFieldEmpty && <p className="text-center">{message}</p>}
 
         <div className="h-14 w-96 flex justify-center items-center gap-3 p-2 border rounded-md border-solid border-red-500">
@@ -235,7 +235,106 @@ function AdminControls() {
         </button>
       </div>
 
-      <div className="flex md:hidden">Mobile</div>
+      <div className="flex md:hidden flex-col justify-center items-center gap-2">
+        {isFieldEmpty && <p className="text-center">{message}</p>}
+
+        <div className="h-14 w-96 flex justify-center items-center gap-3 p-2">
+          <MdLibraryAdd />
+          <input
+            className="p-1 border border-solid"
+            onChange={(e) => setItemName(e.target.value)}
+            type="text"
+            value={itemName}
+            placeholder="Give a name for the item"
+            required
+          />
+        </div>
+
+        <div className="h-14 w-96 flex justify-center items-center gap-3 p-2">
+          <MdOutlinePriceChange />
+          <input
+            className="p-1 border border-solid"
+            onChange={(e) => setItemPrice(e.target.value)}
+            type="text"
+            value={itemPrice}
+            placeholder="What will be price of this item?"
+            required
+          />
+        </div>
+
+        <div className="h-14 w-96 flex justify-center items-center gap-3 p-2">
+          <MdCategory />
+          <select onCanPlay={(e) => setItemCategory(e.target.value)}>
+            <option className="bg-white" value="other">
+              Select a category
+            </option>
+            {categories.map((category) => {
+              return (
+                <option key={category.id} value={category.urlParamName}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className="h-56 w-96 flex flex-col justify-center items-center gap-3 p-2 border rounded-md border-solid border-red-500">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!itemImage ? (
+                <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                  <div className="w-full h-full flex flex-col items-center justify-center">
+                    <MdImage className="text-gray-500 group-hover:text-gray-700 text-3xl" />
+                    <p className="text-gray-500 group-hover:text-gray-700">
+                      Upload an image
+                    </p>
+                  </div>
+                  <input
+                    type="file"
+                    name="upload-image"
+                    accept="image/*"
+                    onChange={(e) => uploadImage(e)}
+                    className="w-0 h-0"
+                  />
+                </label>
+              ) : (
+                <div className="relative h-full">
+                  <img
+                    src={itemImage}
+                    alt=""
+                    className="h-full w-full object-cover"
+                  />
+                  <button
+                    type="button"
+                    className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md"
+                    onClick={() => removeUploadedImage()}
+                  >
+                    <MdDeleteForever className="text-white" />
+                  </button>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+
+        <button
+          className=" bg-red-500 p-2 px-4 rounded-full text-white"
+          onClick={() => saveItemDetails()}
+          type="button"
+        >
+          Submit
+        </button>
+
+        <button
+          className=" bg-red-500 p-2 px-4 rounded-full text-white"
+          onClick={() => clearData()}
+          type="button"
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
 }
